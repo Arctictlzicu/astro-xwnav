62d978712a7a95364ce63ea404f78ab35aad64c8
Arctictlzicu <tlz@tlz.icu>
2025-12-18 23:01:08 +0800

    aaaa

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,20 @@
 /// <reference types="astro/client" />
 declare module 'astro-seo';
+
+interface DifyChatbotConfig {
+  token: string;
+  baseUrl: string;
+  inputs: Record<string, string>;
+  systemVariables: {
+    user_id?: string;
+    conversation_id?: string;
+  };
+  userVariables: {
+    avatar_url?: string;
+    name?: string;
+  };
+}
+
+interface Window {
+  difyChatbotConfig: DifyChatbotConfig;
+}
